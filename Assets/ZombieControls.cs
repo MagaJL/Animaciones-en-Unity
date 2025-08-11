af0e10f4fe53d54e74cd889be65079558fe20345
MagaJL <48716048@est.ort.edu.ar>
2025-08-11 10:39:05 -0300

    Zombie Animation Test

---
+++ Assets/ZombieControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieControls : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.SetBool("IsWalking", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetBool("IsWalking", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetBool("IsWalking", false);
+        }
+    }
+}
